c8f3bd1aeb593f19da1aeb07dd3b2e03e7c4a112
Diego652 <81308873+Diego652@users.noreply.github.com>
2022-05-25 18:58:37 -0400

    Update ShannonF0.6.cpp

--- ShannonF0.6.cpp
+++ ShannonF0.6.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
@@ -16,21 +15,22 @@ struct node {
 };
 
 int comparaFreq(const void *simbolo1,const void *simbolo2);
-void printSimbolos(node *p, int n);
+void printSimbolos(node *p, int n); 
 void Codificar(string DataNOM);
-void Decodificar(string DataNOM);
+void Decodificar();
 void shannonF(int l, int h, node *p);
 void exportarCodigos(node *p, int n);
 
-int main(int argc, char **argv){
+int main(int argc, char ** argv){
 	if(argc != 2){
 		cout << "Error. Debe ejecutarse como ./Tarea archivoNombre.txt " << endl;
 		exit(EXIT_FAILURE);
 	}
 	string DataNOM = argv[1];
+  
+	//Codificar(DataNOM);
+  Decodificar();
 
-	Codificar(DataNOM);
-  Decodificar(DataNOM);
 }
 
 void Codificar(string DataNOM){
@@ -61,25 +61,26 @@ void Codificar(string DataNOM){
     pesoTotal+=element.second;   			// element.second es la frecuencia (valor) y se suma a pesoTotal para tener el numero total de caracteres
   }
   
+  
   n=freq.size();
-  node *p = new node[n];
+  node *p = new node[n]; // crear el arreglo p con char y freq
   i=0;
   for (auto element:freq){ 			
     p[i].sym=element.first;
 		p[i].pro=((float)element.second)/pesoTotal;
-		i++; 		
+    i++; 		
   }
   
-  qsort(p, n, sizeof(node), comparaFreq);
-  shannonF(0, n-1, p);
+  // qsort sizeof(node) => tamano de bits que ocupa la estructura node
+  qsort(p, n, sizeof(node), comparaFreq); // (1) queda p ordenado descendentemente en base a freq(p) de cada char
+  shannonF(0, n-1, p); 
   printSimbolos(p, n);
-  
-  /////////////////////////////////////////////
-  ofstream TextoOutput ("codificado.txt");
+
+  ofstream TextoOutput ("codificado.txt"); 
   exportarCodigos(p, n);
-  
+
   texto.open(dir,ios::in);
-  if (texto.is_open()){ 		 	 // si el archivo se abrio (validacion para no tener errores)
+  if (texto.is_open()){ 		 	   // si el archivo se abrio (validacion para no tener errores)
       while(getline(texto, linea)){ //un while que itera por las lineas del texto dejandolas en la variable linea
         n=linea.length();  			 // en n se almacena el largo de linea
         for(i=0;i<n;i++){  		 //se recorre el string de largo n
@@ -89,16 +90,17 @@ void Codificar(string DataNOM){
         TextoOutput << endl;
       }
       texto.close();    						   //se cierra el texto a leer
+      
   }
   
   TextoOutput.close();
   /////////////////////////////////////////////
 };
 
-void Decodificar(string DataNOM){
+void Decodificar(){
   int i,n,j,z;
   FILE *CodigosImport;
-  string dir = "codificado.txt", linea, CodigoSUM, codificado;
+  string dir = "codificado.txt", linea, CodigoSUM, codificado; //
   fstream texto; 
 
   CodigosImport = fopen("Codigos.txt", "r");
@@ -109,7 +111,7 @@ void Decodificar(string DataNOM){
  
   for(i=0; i<n; i++){
     fscanf(CodigosImport, "%c %f %s", &(p[i].sym), &(p[i].pro), (codificado).c_str());
-    p[i].codigo= (codificado).c_str();
+    p[i].codigo = (codificado).c_str();
     fgetc(CodigosImport);
   }
   //printSimbolos(p, n);
@@ -117,7 +119,7 @@ void Decodificar(string DataNOM){
   ofstream TextoOutput ("decodificado.txt");
 
   texto.open(dir,ios::in);
-  if (texto.is_open()){ 		 	 // si el archivo se abrio (validacion para no tener errores)
+  if (texto.is_open()){ // si el archivo se abrio (validacion para no tener errores)
       while(getline(texto, linea)){ //un while que itera por las lineas del texto dejandolas en la variable linea
         j=linea.length();  			 // en n se almacena el largo de linea
         CodigoSUM="";
@@ -131,7 +133,7 @@ void Decodificar(string DataNOM){
             }
           }
         }
-        TextoOutput << endl;
+      TextoOutput << endl;
       }
     texto.close();    						   //se cierra el texto a leer
   }
@@ -139,12 +141,14 @@ void Decodificar(string DataNOM){
   /////////////////////////////////////////////
 };
 
-
 int comparaFreq(const void *simbolo1,const void *simbolo2){
-  float prob1 = (*(node*)simbolo1).pro;
-  float prob2 = (*(node*)simbolo2).pro;
-
-  if (prob1 < prob2) return 1;
+  float prob1 = (*(node*)simbolo1).pro; // probabilidad simbolo 1
+  float prob2 = (*(node*)simbolo2).pro; // probabilidad simbolo 2
+  
+  // si son iguales queda igual
+  // devuelve un bool si p1 < p2 (positivo)
+  // devuelve un bool si p1 > p2 (negativo)
+  if (prob1 < prob2) return 1;      
   else if(prob1 > prob2) return -1;
   return 0;
 };
@@ -156,30 +160,32 @@ void printSimbolos(node *p, int n){
 };
 
 void shannonF(int l, int r, node *p){ 
-  if(l==r) { return; }
-  else if(r-l == 1){
-    p[l].codigo += "1";
-    p[r].codigo += "0";
-  }
-  else {
-  float sumaT=0, sumaP=p[l].pro, sumaMitad;
+  float sumaT=0, sumaP = p[l].pro, sumaMitad;
   int i,z;
-
-  for (i=l; i<=r; i++) {    sumaT += p[i].pro;  }
-
+  for (i=l; i<=r; i++){
+    sumaT += p[i].pro;
+    } 
   sumaMitad = sumaT * 0.5f;
-  for (i=l; i<=r;) {
-    p[i].codigo= p[i].codigo + "1";
+  if(l==r){return; } // l y r son la posicion de intervalo izq y intervalo der
+  // si son iguales retorna
+  else if(r-l == 1){
+    p[l].codigo += "1"; // si hay dos elementos en el intervalo [l,r]
+    p[r].codigo += "0"; // le asigna un 1 al izq y un 0 al derecho
+  }
+  
+  else {
+  for (i = l; i <= r;) {
+    p[i].codigo = p[i].codigo + "0";
     i++;
-    sumaP += p[i].pro;
-    if (sumaP > sumaMitad) break;
+    sumaP += p[i].pro; 
+    //cout<< p[i].codigo <<" p[i]"<< endl;
+    if (sumaP >= sumaMitad) break; 
   }
-
-  for (z=i; z<=r ;z++) {
-    p[z].codigo= p[z].codigo + "0";
+  for (z = i; z <= r; z++) {
+    p[z].codigo = p[z].codigo + "1";
+    //cout<< p[z].codigo << "p[z] "<< endl;
   }
-
-  shannonF(l, i-1, p);
+  shannonF(l, i-1, p); 
   shannonF(i, r, p);
   }
 };
